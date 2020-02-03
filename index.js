79ee1f2355a0be5169bbf7a72014f421fdaab3ae
Angelo Fonseca <angelofonseca@Mac-mini-de-Angelo.local>
2020-02-02 23:57:38 -0300

    utilizando vetor como parâmetro

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require("express");
 
 const server = express();
 
+const users = ["Angelo", "Aline", "Alexandre", "Joaquim", "Alaíde"];
+
 server.get("/users", (req, res) => {
   const { name } = req.query;
   res.send(`Hello, ${name}!!!`);
 });
 
-server.get("/users/:id", (req, res) => {
-  const { id } = req.params;
-  res.send(`Hello, user number ${id}!!!`);
+server.get("/users/:index", (req, res) => {
+  const { index } = req.params;
+  res.send(`Hello, ${users[index]}!!!`);
 });
 
 server.listen(3000);
