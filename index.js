7316a676b7ca55f8ed37db960f397d8076280a6b
Angelo Fonseca <angelofonseca@Mac-mini-de-Angelo.local>
2020-02-02 22:24:15 -0300

    query params e route params

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const express = require("express");
 const server = express();
 
 server.get("/teste", (req, res) => {
-  res.send("Hello World!!!");
+  const { name } = req.query;
+  res.send(`Hello, ${name}!!!`);
+});
+
+server.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  res.send(`Hello, user number ${id}!!!`);
 });
 
 server.listen(3000);
